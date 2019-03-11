79e82221fe1757a206b57c8a28f3dcffe5db5dc2
Pithani.Kishan Dev <44198442+kishandev1999@users.noreply.github.com>
2019-03-11 23:36:06 +0530

    initial commit

---
+++ arithematic operations program.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+float x,y,A,S,M,D;
+printf("the value of a and b are :");
+scanf("%f%f",&x,&y);
+A=x+y;
+S=x-y;
+M=x*y;
+D=x/y;
+printf("the value of Addition,subtraction,Multiplication,Division is %f\n%f\n%f\n%f",A,S,M,D);
+getch();
+
